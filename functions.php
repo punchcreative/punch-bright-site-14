9de7e3b82cd2083b8e4b1c902ce281e36106dd5c
Erik Kroon <mail@punchcreative.nl>
2014-03-05 08:46:00 +0100

    Fix shortcode BS2 span

--- functions.php
+++ functions.php
@@ -32,8 +32,8 @@ if ( ! isset( $content_width ) ) $content_width = 580;
 /************* THUMBNAIL SIZE OPTIONS *************/
 
 // Thumbnail sizes
-add_image_size( 'wpbs-featured', 780, 300, true );
-add_image_size( 'wpbs-featured-home', 970, 311, true);
+add_image_size( 'wpbs-featured', 970, 180, true );
+add_image_size( 'wpbs-featured-home', 970, 220, true);
 add_image_size( 'wpbs-featured-carousel', 970, 400, true);
 
 /* 
